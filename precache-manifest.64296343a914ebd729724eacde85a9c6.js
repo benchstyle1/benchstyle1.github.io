7568f1fcd2ec4f2628f21404f33d449275a750b6
benchstyle1 <47250962+canijke@users.noreply.github.com>
2020-10-26 20:55:13 +0300

    deploy

---
+++ precache-manifest.64296343a914ebd729724eacde85a9c6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6611b1ff531bfbcd990",
+    "url": "/css/app.084d5f30.css"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/fonts/Oswald.a6e5446a.ttf"
+  },
+  {
+    "revision": "3edf5a48987b589c0cfa4a7cc84fee49",
+    "url": "/img/MST.3edf5a48.jpg"
+  },
+  {
+    "revision": "a248befb3d2ff5d03496245d8a90281f",
+    "url": "/img/canvas.a248befb.jpg"
+  },
+  {
+    "revision": "a48d8bab7d620b3a28bc270c53ad2735",
+    "url": "/img/currency.a48d8bab.jpg"
+  },
+  {
+    "revision": "e8d112c909a0c6274c21830e371e59fa",
+    "url": "/img/fashion.e8d112c9.jpg"
+  },
+  {
+    "revision": "e30803b3f34e5ab01441ae8d35304cce",
+    "url": "/img/gallery.e30803b3.jpg"
+  },
+  {
+    "revision": "5fc3279973ba392aa626cfa288e4c4c3",
+    "url": "/img/image.5fc32799.jpg"
+  },
+  {
+    "revision": "d8bb8c419d73640467f835c5322f337e",
+    "url": "/img/todoList.d8bb8c41.jpg"
+  },
+  {
+    "revision": "112d1ec6d67425f2587dafcc60f5a065",
+    "url": "/img/transport.112d1ec6.jpg"
+  },
+  {
+    "revision": "fc6ef8a9c472f715e0f6df8275fbac88",
+    "url": "/img/weather.fc6ef8a9.jpg"
+  },
+  {
+    "revision": "169de33e4e9a2e6c2498efc61c84011c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6611b1ff531bfbcd990",
+    "url": "/js/app.6037aac5.js"
+  },
+  {
+    "revision": "e7723eb2ca348c78c5d9",
+    "url": "/js/chunk-vendors.c043f2e6.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
